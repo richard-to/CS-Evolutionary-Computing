b39674177da48f49f33173ec91a96212459baca3
richard-to <gohn76@gmail.com>
2013-01-29 13:50:09 -0900

    Add IRandom interface with wrapper and mock implementations

---
+++ ex0/src/to/richard/IRandom.java
@@ -0,0 +1,28 @@
+package to.richard;
+
+/**
+ * Author: Richard To
+ * Date: 1/29/13
+ */
+
+/**
+ * Simplified interface for generating random numbers.
+ *
+ * This interface implements what is needed for my SGA.
+ * This interface is needed to allow mock random objects for testing.
+ */
+public interface IRandom {
+
+    /**
+     * Returns the next pseudorandom, uniformly distributed int value from this random number generator's sequence.
+     * @param n
+     * @return
+     */
+    public double nextInt(int n);
+
+    /**
+     * Returns the next pseudorandom, uniformly distributed double value between 0.0 and 1.0 from this random number generator's sequence.
+     * @return
+     */
+    public double nextDouble();
+}
---
+++ ex0/src/to/richard/Random.java
@@ -0,0 +1,39 @@
+package to.richard;
+
+/**
+ * Author: Richard To
+ * Date: 1/29/13
+ */
+
+/**
+ * Wrapper class for java.util.Random.
+ *
+ * This class only provides a subset of the functionality
+ * and implements the IRandom interface.
+ */
+public class Random implements IRandom {
+
+    private java.util.Random _random;
+
+    public Random() {
+        _random = new java.util.Random();
+    }
+
+    /**
+     * Construct a random number generator with specific seed.
+     * @param seed
+     */
+    public Random(long seed) {
+        _random = new java.util.Random(seed);
+    }
+
+    @Override
+    public double nextInt(int n) {
+        return _random.nextInt(n);
+    }
+
+    @Override
+    public double nextDouble() {
+        return _random.nextDouble();
+    }
+}
