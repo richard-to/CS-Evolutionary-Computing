aa94c10a35c7725fd4e9b5cec00d8fd7a0cf2fbb
richard-to <gohn76@gmail.com>
2013-01-29 14:16:37 -0900

    Update parent selector class to use IRandom. Implement selectParents method.

--- ex0/src/to/richard/ParentSelector.java
+++ ex0/src/to/richard/ParentSelector.java
@@ -1,27 +1,52 @@
 package to.richard;
 
+import java.util.ArrayList;
+
 /**
  * Author: Richard To
  * Date: 1/29/13
  */
 
-import java.util.ArrayList;
-import java.util.Random;
-
 /**
- * Parent Selector
+ * Parent Selector emulates simple parent selection.
+ *
+ * Selects genotypes to act as parents for recombination step.
  */
 public class ParentSelector {
 
-    private Random _random;
+    private IRandom _random;
 
-    public ParentSelector(Random random) {
+    public ParentSelector(IRandom random) {
         _random = random;
     }
 
-    public void selectParents(GenePool genePool, int parentsCount) {
+    public ArrayList<GenotypeParents> selectParents(GenePool genePool, int parentsCount) {
+
+        ArrayList<GenotypeParents> genotypeParentsList = new ArrayList<GenotypeParents>();
+        Genotype parent1 = null;
+        Genotype parent2 = null;
+
         ArrayList<Genotype> genotypeList = genePool.getGenotypeList();
+        double totalFitness = 0.0;
+
         ProbabilityDistribution<Genotype> genotypeProbabilityDistribution =
                 new ProbabilityDistribution<Genotype>(_random);
+        double parentProbability = 0.0;
+
+        for (Genotype genotype : genotypeList) {
+            totalFitness += genotype.getFitness();
+        }
+
+        for (Genotype genotype : genotypeList) {
+            parentProbability = genotype.getFitness() / totalFitness;
+            genotypeProbabilityDistribution.add(parentProbability, genotype);
+        }
+
+        for (int i = 0; i < parentsCount; i++) {
+            parent1 = genotypeProbabilityDistribution.getRandom().clone();
+            parent2 = genotypeProbabilityDistribution.getRandom().clone();
+            genotypeParentsList.add(new GenotypeParents(parent1, parent2));
+        }
+        return genotypeParentsList;
     }
 }
