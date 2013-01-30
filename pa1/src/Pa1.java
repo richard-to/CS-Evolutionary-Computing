b05833727d54e26e23988ee617165329e90d2e95
richard-to <gohn76@gmail.com>
2013-01-30 02:28:50 -0900

    Add boilerplate for Pa1

---
+++ pa1/src/Pa1.java
@@ -0,0 +1,20 @@
+/**
+ * Author: Richard To
+ * Date: 1/30/13
+ */
+
+/**
+ * Main class for to simulate TSP problem using Genetic Algorithm.
+ *
+ * Recombination algorithms: PMX, OX, EX, CX
+ */
+public class Pa1 {
+
+    /**
+     * Main class
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
