a6b530e8b6031343939bb26009198f27a6f67e0a
richard-to <gohn76@gmail.com>
2013-02-06 00:31:43 -0900

    Add few more tests for cost matrix exceptions

--- pa1/src/to/richard/tsp/test/CostMatrixTest.java
+++ pa1/src/to/richard/tsp/test/CostMatrixTest.java
@@ -34,4 +34,27 @@ public class CostMatrixTest {
         assertEquals(20, costMatrix.getCost(0, 1));
         assertEquals("1", costMatrix.getCityName(1));
     }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testCityNameIndexBounds() throws Exception {
+        int[][] matrix = {{10, 20}, {30, 40}};
+        String[] cityNames = {"City0", "City1"};
+        CostMatrix costMatrix = new CostMatrix(matrix, cityNames);
+        costMatrix.getCityName(5);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testIndexOutOfBounds() throws Exception {
+        int[][] matrix = {{10, 20}, {30, 40}};
+        String[] cityNames = {"City0", "City1"};
+        CostMatrix costMatrix = new CostMatrix(matrix, cityNames);
+        costMatrix.getCost(5, 2);
+    }
+
+    @Test(expected = Exception.class)
+    public void testCityNamesNotEqualToMatrix() throws Exception {
+        int[][] matrix = {{10, 20}, {30, 40}};
+        String[] cityNames = {"City0", "City1", "City3"};
+        CostMatrix costMatrix = new CostMatrix(matrix, cityNames);
+    }
 }
