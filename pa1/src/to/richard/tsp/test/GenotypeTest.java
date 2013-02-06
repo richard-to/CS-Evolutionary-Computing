921a445e0b0a37e75127b8c40d5b2470eff0c1e7
richard-to <gohn76@gmail.com>
2013-02-06 00:04:29 -0900

    Add more tests on Mutable/Immutable Genotype

--- pa1/src/to/richard/tsp/test/GenotypeTest.java
+++ pa1/src/to/richard/tsp/test/GenotypeTest.java
@@ -37,4 +37,35 @@ public class GenotypeTest {
         assertEquals(3, genotype.getAllele(3));
         assertEquals(10, mutableGenotype.getAllele(3));
     }
+
+    @Test
+    public void testSetAllele() throws Exception {
+        MutableGenotype mutableGenotype = new MutableGenotype(3);
+        mutableGenotype.setAllele(10, 1);
+        assertEquals(0, mutableGenotype.getAllele(0));
+        assertEquals(10, mutableGenotype.getAllele(1));
+        mutableGenotype.setAllele(20, 1);
+        assertEquals(20, mutableGenotype.getAllele(1));
+    }
+
+    @Test
+    public void testCopy() throws Exception {
+        MutableGenotype mutableGenotype =
+            new MutableGenotype(new int[]{0, 1, 2, 3, 4});
+        Genotype genotype = mutableGenotype.copy();
+        mutableGenotype.setAllele(10, 1);
+        assertEquals(1, genotype.getAllele(1));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testIndexOutOfBoundsNegative() {
+        Genotype genotype = new Genotype(new int[]{0, 1, 2, 3, 4});
+        genotype.getAllele(-1);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testIndexOutOfBoundsOver() {
+        Genotype genotype = new Genotype(new int[]{0, 1, 2, 3, 4});
+        genotype.getAllele(20);
+    }
 }
