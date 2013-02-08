72845884f58b73ecec2396203f8d060510d8427a
richard-to <gohn76@gmail.com>
2013-02-07 15:25:18 -0900

    Fix bug where start greater than end

--- pa1/src/to/richard/tsp/InversionMutation.java
+++ pa1/src/to/richard/tsp/InversionMutation.java
@@ -15,11 +15,18 @@ public class InversionMutation implements IMutationStrategy {
     public Genotype mutate(Genotype genotype, IRandom random) {
         int start = random.nextInt(genotype.length());
         int end = random.nextInt(genotype.length());
+
+        if (start > end) {
+            int temp = start;
+            start = end;
+            end = temp;
+        }
+
         int length = end - start + 1;
         MutableGenotype mutableGenotype = genotype.copyMutable();
         for (int i = 0; i < length; i++) {
             mutableGenotype.setAllele(genotype.getAllele(start + i), end - i);
         }
-        return genotype;
+        return mutableGenotype.copy();
     }
 }
