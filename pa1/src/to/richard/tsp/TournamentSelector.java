78cbe847ec1bdd21ee1c255e753148427aafd07a
richard-to <gohn76@gmail.com>
2013-02-10 14:21:53 -0900

    Finish Tournament selector implemention with deterministic tournaments

--- pa1/src/to/richard/tsp/TournamentSelector.java
+++ pa1/src/to/richard/tsp/TournamentSelector.java
@@ -1,7 +1,6 @@
 package to.richard.tsp;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -10,40 +9,46 @@ import java.util.List;
  */
 
 /**
- * Parent selection using tournament selection.
+ * Parent selection using tournament selection. This is a deterministic tournament
+ * so probability that the best genotype will be selected is 1.0.
  *
- * Randomly select two genotypes. The one with best fitness is selected.
+ * Randomly select k genotypes. The one with best fitness is selected.
  * This is repeated until we have reached the specified population size.
  *
  * Population size is currently determined by the size of the genotype list
  * passed into the selectParents method.
- *
- * Note: I think in Tournament selection more than two genotypes can be selected,
- * but I am just going with two for now.
  */
 public class TournamentSelector implements ISelector {
     private IRandom _random;
+    private int _tournamentSize;
+    private IFitnessComparator _fitnessComparator;
     private FitnessEvaluator _fitnessEvaluator;
 
-    public TournamentSelector(IRandom random, FitnessEvaluator fitnessEvaluator) {
-        _random = random;
+    public TournamentSelector(int tournamentSize, FitnessEvaluator fitnessEvaluator,
+                              IFitnessComparator fitnessComparator, IRandom random) {
+        _tournamentSize = tournamentSize;
         _fitnessEvaluator = fitnessEvaluator;
+        _fitnessComparator = fitnessComparator;
+        _random = random;
     }
 
     public List<Genotype> selectParents(List<Genotype> genotypes) {
         int populationSize = genotypes.size();
         ArrayList<Genotype> parentGenotypes = new ArrayList<Genotype>();
-        Genotype parent1;
-        Genotype parent2;
+        ArrayList<Pair<Double, Genotype>> tournament = new ArrayList<Pair<Double, Genotype>>();
+        Pair<Double, Genotype> mostFitGenotype = null;
 
         while (parentGenotypes.size() < populationSize) {
-            parent1 = genotypes.get(_random.nextInt(populationSize));
-            parent2 = genotypes.get(_random.nextInt(populationSize));
-            if (_fitnessEvaluator.evaluate(parent1) > _fitnessEvaluator.evaluate(parent2)) {
-                parentGenotypes.add(parent1);
-            } else {
-                parentGenotypes.add(parent2);
+            tournament.clear();
+            for (int i = 0; i < _tournamentSize; i++) {
+                tournament.add(_fitnessEvaluator.evaluateAsPair(
+                        genotypes.get(_random.nextInt(populationSize))));
             }
+            mostFitGenotype = tournament.get(0);
+            for (int i = 1; i < tournament.size(); i++) {
+                mostFitGenotype = _fitnessComparator.compare(mostFitGenotype, tournament.get(i));
+            }
+            parentGenotypes.add(mostFitGenotype.getSecondValue());
         }
         return (List<Genotype>)parentGenotypes;
     }
