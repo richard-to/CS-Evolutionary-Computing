e2e2b69396914bc6ea6d850126c85f536ebf802e
richard-to <gohn76@gmail.com>
2013-02-09 16:44:30 -0900

    Add test for book example

--- pa1/src/to/richard/tsp/test/RecombinatorTest.java
+++ pa1/src/to/richard/tsp/test/RecombinatorTest.java
@@ -81,4 +81,43 @@ public class RecombinatorTest {
         assertEquals("0123465", newGenotypes.get(0).toString());
         assertEquals("0153246", newGenotypes.get(1).toString());
     }
+
+    @Test
+    public void testRecombinePMXSame() throws Exception {
+
+        MockRandom random = new MockRandom();
+        ArrayList<Integer> sequenceInt = new ArrayList<Integer>(Arrays.asList(
+                new Integer[]{1,5}));
+        random.setIntegerSequence(sequenceInt);
+
+        PartiallyMappedCrossover pmx = new PartiallyMappedCrossover();
+
+        Genotype g1 = new Genotype(new Allele[]{new Allele(1),new Allele(2),new Allele(3),
+                new Allele(4),new Allele(5),new Allele(6),new Allele(7),new Allele(8),new Allele(9)});
+        Genotype g2 = new Genotype(new Allele[]{new Allele(1),new Allele(2),new Allele(3),
+                new Allele(4),new Allele(5),new Allele(6),new Allele(7),new Allele(8),new Allele(9)});
+        List<Genotype> newGenotypes = pmx.crossover(g1, g2, random);
+
+        assertEquals("123456789", newGenotypes.get(0).toString());
+        assertEquals("123456789", newGenotypes.get(1).toString());
+    }
+
+    @Test
+    public void testRecombinePMXBookExample() throws Exception {
+
+        MockRandom random = new MockRandom();
+        ArrayList<Integer> sequenceInt = new ArrayList<Integer>(Arrays.asList(
+                new Integer[]{2,7}));
+        random.setIntegerSequence(sequenceInt);
+
+        PartiallyMappedCrossover pmx = new PartiallyMappedCrossover();
+
+        Genotype g1 = new Genotype(new Allele[]{new Allele(1),new Allele(2),new Allele(3),
+                new Allele(4),new Allele(5),new Allele(6),new Allele(7),new Allele(8),new Allele(9)});
+        Genotype g2 = new Genotype(new Allele[]{new Allele(9),new Allele(3),new Allele(7),
+                new Allele(8),new Allele(2),new Allele(6),new Allele(5),new Allele(1),new Allele(4)});
+        List<Genotype> newGenotypes = pmx.crossover(g1, g2, random);
+
+        assertEquals("932456718", newGenotypes.get(0).toString());
+    }
 }
