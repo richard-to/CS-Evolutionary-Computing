d1c9021162229b54faa32f55a9c4b3fb96bbb862
richard-to <gohn76@gmail.com>
2013-02-11 12:04:19 -0900

    Add test for replace worst

--- pa1/src/to/richard/tsp/test/SurvivalSelectorTest.java
+++ pa1/src/to/richard/tsp/test/SurvivalSelectorTest.java
@@ -77,4 +77,35 @@ public class SurvivalSelectorTest {
         assertEquals(parents.get(0), nextGeneration.get(0));
         assertEquals(offspring.get(0), nextGeneration.get(1));
     }
+
+    @Test
+    public void testReplaceWorst() throws Exception {
+        int[][] costMatrixArray = {{20, 30, 10, 75}, {40, 21, 23, 200}, {42, 11, 101, 2}, {12, 211, 11, 56}};
+        CostMatrix costMatrix = new CostMatrix(costMatrixArray);
+
+        FitnessMinimizationComparator comparator = new FitnessMinimizationComparator();
+        FitnessEvaluator fitnessEvaluator = new FitnessEvaluator(costMatrix);
+
+        ArrayList<Genotype> parents = new ArrayList<Genotype>() {{
+            // Fitness 67  = 867 = 284 = .604
+            add(new Genotype(new Allele[]{new Allele(1), new Allele(2), new Allele(3)}));
+
+            // Fitness 233 = 701 = 118 = .25
+            add(new Genotype(new Allele[]{new Allele(2), new Allele(1), new Allele(3)}));
+        }};
+
+        ArrayList<Genotype> offspring = new ArrayList<Genotype>() {{
+            // Fitness 283 = 651 = 68 = .14
+            add(new Genotype(new Allele[]{new Allele(1), new Allele(3), new Allele(2)}));
+
+            // Fitness 351 = 583 = 0
+            add(new Genotype(new Allele[]{new Allele(3), new Allele(1), new Allele(2)}));
+        }};
+
+        ISurvivorSelector survivorSelector = new ReplaceWorstSurvivorSelection(fitnessEvaluator, comparator);
+        List<Genotype> nextGeneration = survivorSelector.replace(parents, offspring);
+
+        assertEquals(parents.get(0), nextGeneration.get(0));
+        assertEquals(parents.get(1), nextGeneration.get(1));
+    }
 }
