c0f227d56f09a81106e2a8992a27b971ac628579
richard-to <gohn76@gmail.com>
2013-02-11 22:55:32 -0900

    Add bug fix for FPSWindowing. If all have same fitness, then do nothing

--- pa1/src/to/richard/tsp/FPSWindowing.java
+++ pa1/src/to/richard/tsp/FPSWindowing.java
@@ -18,6 +18,7 @@ import java.util.List;
  *
  * 1. Find the lowest fitness
  * 2. Subtract that fitness value from population.
+ * 3. If whole population has same fitness, then leave list unchanged.
  *
  * Note that windowing only works when high fitness is considered best.
  * If working with lowest fitness, then FPSMinimization may need to be used
@@ -33,18 +34,26 @@ public class FPSWindowing implements IFPSTransform {
 
     @Override
     public List<Pair<Double, Genotype>> transform(List<Pair<Double, Genotype>> genotypeFitnessList) {
+
+        boolean leastFitChanged = false;
+
         Pair<Double, Genotype> leastFitGenotype = genotypeFitnessList.get(0);
         ArrayList<Pair<Double, Genotype>> newList = new ArrayList<Pair<Double, Genotype>>();
 
         for (Pair<Double, Genotype> pair : genotypeFitnessList) {
-            if(_comparator.compare(leastFitGenotype, pair) > -1) {
+            if (_comparator.compare(leastFitGenotype, pair) > 0) {
                 leastFitGenotype = pair;
+                leastFitChanged = true;
             }
         }
 
-        for (Pair<Double, Genotype> pair : genotypeFitnessList) {
-            newList.add(new Pair<Double, Genotype>(
-                    pair.getFirstValue() - leastFitGenotype.getFirstValue(), pair.getSecondValue()));
+        if (leastFitChanged) {
+            for (Pair<Double, Genotype> pair : genotypeFitnessList) {
+                newList.add(new Pair<Double, Genotype>(
+                        pair.getFirstValue() - leastFitGenotype.getFirstValue(), pair.getSecondValue()));
+            }
+        } else {
+            newList = new ArrayList(genotypeFitnessList);
         }
 
         return newList;
