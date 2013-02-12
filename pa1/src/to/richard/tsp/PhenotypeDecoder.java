91002c4feb8e5805097e9f7037537a37648dba59
richard-to <gohn76@gmail.com>
2013-02-12 13:09:02 -0900

    Add Phenotype decoder class

---
+++ pa1/src/to/richard/tsp/PhenotypeDecoder.java
@@ -0,0 +1,42 @@
+package to.richard.tsp;
+
+/**
+ * Author: Richard To
+ * Date: 2/12/13
+ */
+
+import java.util.List;
+
+/**
+ * Returns phenotype of genotype.
+ *
+ * This should be named better, but this works
+ * specifically for the TSP problem and the following output:
+ *
+ * Anchorage/Georgia(200):Georgia/Seattle(300)
+ */
+public class PhenotypeDecoder {
+
+    private CostMatrix _costMatrix;
+
+    public PhenotypeDecoder(CostMatrix costMatrix) {
+        _costMatrix = costMatrix;
+    }
+
+    public String decode(Genotype genotype) {
+        StringBuilder genotypeStringBuilder = new StringBuilder();
+        Allele home = new Allele(0);
+        Allele fromAllele = home;
+        int count = 1;
+        for (Allele toAllele : genotype) {
+            genotypeStringBuilder.append(String.format("%2d. %s/%s(%.0f)%n", count++,
+                    _costMatrix.getAlleleName(fromAllele), _costMatrix.getAlleleName(toAllele),
+                    _costMatrix.getCost(fromAllele, toAllele)));
+            fromAllele = toAllele;
+        }
+        genotypeStringBuilder.append(String.format("%d. %s/%s(%.0f)", count,
+                _costMatrix.getAlleleName(fromAllele), _costMatrix.getAlleleName(home),
+                _costMatrix.getCost(fromAllele, home)));
+        return genotypeStringBuilder.toString();
+    }
+}
