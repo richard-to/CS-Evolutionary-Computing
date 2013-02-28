a737ad746ff357bf4db68c7e21b975fcda1e7ca9
richard-to <gohn76@gmail.com>
2013-02-28 14:15:27 -0900

    Add rough discrete global recombination function. Add initial population generator
    Still in unusable form

--- pa2/pa2.py
+++ pa2/pa2.py
@@ -1,15 +1,28 @@
 from math import e, pi, sin
-from random import gauss, seed, uniform
+from random import gauss, sample, seed, uniform
 
-def calcFitness(x1, x2):
+def fitness(x1, x2):
     return 21.5 + x1 * sin(4.0 * pi * x1) + x2 * sin(20.0 * pi * x2)
 
 def checkRange(x, min, max):
     return min <= x <= max
 
-def createGeneration0(mu):
+def createGeneration0(numParents, x1Range, x2Range, initialSigma):
+    return [[uniform(*x1Range), uniform(*x2Range), initialSigma, initialSigma] for i in range(numParents)]
 
-def selectSurvivors(parents, offspring):
+def discreteGlobalRecombination(numOffspring, parents):
+    offspring = []
+    parentIndexes = range(len(parents))
+    for i in range(numOffspring):
+        child = []
+        for i in range(len(parents[0])):
+            selectedParents = sample(parentIndexes, 2)
+            chosenParent = sample(selectedParents, 1)
+            child.append(parents[chosenParent[0]][i])
+        offspring.append(child)
+    return offspring
+
+def surviorSelection(parents, offspring):
     return parents
 
 # gene = [x1, x2, o1, o2]
@@ -18,10 +31,18 @@ def selectSurvivors(parents, offspring):
 
 if __name__ == '__main__':
 
-numParents = 3
-numOffspring = 21
+    numParents = 3
+    numOffspring = 21
+    x1Range = (-3, 12)
+    x2Range = (4, 6)
+    initialSigma = 1.0
 
-# create x(3) parents (values with default sigma of 1)
+    parents = createGeneration0(numParents, x1Range, x2Range, initialSigma)
+    print parents
+
+    print discreteGlobalRecombination(numOffspring, parents)
+
+    # create x(3) parents (values with default sigma of 1)
 
 
-# create y(21) children (intermediate and global)
+    # create y(21) children (intermediate and global)
