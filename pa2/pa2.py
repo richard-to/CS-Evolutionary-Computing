cb41d4ec375b8e16be5dc84530bb76da15f0cab9
richard-to <gohn76@gmail.com>
2013-02-28 15:06:48 -0900

    Add global discrete and intermediate recombination implementations

--- pa2/pa2.py
+++ pa2/pa2.py
@@ -10,29 +10,43 @@ def checkRange(x, min, max):
 def createGeneration0(numParents, x1Range, x2Range, initialSigma):
     return [[uniform(*x1Range), uniform(*x2Range), initialSigma, initialSigma] for i in range(numParents)]
 
-def discreteGlobalRecombination(numOffspring, parents):
+def recombination(parents, numVariables, numOffspring, vRecombination, sRecombination):
+    vOffspring = vRecombination(parents, numOffspring, numVariables)
+    sOffspring = sRecombination(parents, numOffspring, numVariables, numVariables)
+    return [vOffspring[i] + sOffspring[i] for i in range(numOffspring)]
+
+def discreteGlobalRecombination(parents, numOffspring, numVariables, offset=0):
     offspring = []
     parentIndexes = range(len(parents))
     for i in range(numOffspring):
         child = []
-        for i in range(len(parents[0])):
+        for i in range(numVariables):
             selectedParents = sample(parentIndexes, 2)
-            chosenParent = sample(selectedParents, 1)
-            child.append(parents[chosenParent[0]][i])
+            chosenParent = sample(selectedParents, 1)[0]
+            child.append(parents[chosenParent][i + offset])
+        offspring.append(child)
+    return offspring
+
+def intermediateGlobalRecombination(parents, numOffspring, numVariables, offset=0):
+    offspring = []
+    parentIndexes = range(len(parents))
+    for i in range(numOffspring):
+        child = []
+        for i in range(numVariables):
+            selectedParents = sample(parentIndexes, 2)
+            newValue = (parents[selectedParents[0]][i + offset] + parents[selectedParents[1]][i + offset]) / 2.0
+            child.append(newValue)
         offspring.append(child)
     return offspring
 
 def surviorSelection(parents, offspring):
     return parents
-
-# gene = [x1, x2, o1, o2]
-# gauss(mu, sigma)
-# uniform(a, b)
-
+    
 if __name__ == '__main__':
 
     numParents = 3
     numOffspring = 21
+    numVariables = 2
     x1Range = (-3, 12)
     x2Range = (4, 6)
     initialSigma = 1.0
@@ -40,8 +54,9 @@ if __name__ == '__main__':
     parents = createGeneration0(numParents, x1Range, x2Range, initialSigma)
     print parents
 
-    print discreteGlobalRecombination(numOffspring, parents)
-
+    offspring = recombination(parents, numVariables, numOffspring, 
+        discreteGlobalRecombination, intermediateGlobalRecombination)
+    print offspring
     # create x(3) parents (values with default sigma of 1)
 
 
