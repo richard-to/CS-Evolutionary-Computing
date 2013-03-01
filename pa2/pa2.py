fad43ca6238604f43809d3e1915f5b728bde6575
richard-to <gohn76@gmail.com>
2013-02-28 23:16:47 -0900

    Add mutation function

--- pa2/pa2.py
+++ pa2/pa2.py
@@ -1,8 +1,14 @@
-from math import e, pi, sin
+from math import e, pi, pow, sin, sqrt
 from random import gauss, sample, seed, uniform
 
 def fitness(x1, x2):
     return 21.5 + x1 * sin(4.0 * pi * x1) + x2 * sin(20.0 * pi * x2)
+
+def overallLearningRate(n):
+    return 1 / sqrt(2 * n)
+
+def specificLearningRate(n):
+    return 1 / sqrt(2 * sqrt(n))
 
 def checkRange(x, min, max):
     return min <= x <= max
@@ -39,9 +45,27 @@ def intermediateGlobalRecombination(parents, numOffspring, numVariables, offset=
         offspring.append(child)
     return offspring
 
+def mutation(offspring, stepSize, numVariables):
+    overall = overallLearningRate(stepSize) * gauss(0, 1)
+    sMutatedOffspringList = []
+    t = []
+    for child in offspring:
+        sMutatedOffspring = []
+        for i in range(numVariables):
+            sMutatedOffspring.append(child[i + numVariables] * e**(overall + specificLearningRate(stepSize) * gauss(0, 1)))
+        sMutatedOffspringList.append(sMutatedOffspring)
+
+    for i in range(len(offspring)):
+        tc = []
+        for v in range(numVariables):
+            tc.append(offspring[i][v] + sMutatedOffspringList[i][v] * gauss(0, 1))
+        t.append(tc)
+
+    return [t[i] + sMutatedOffspringList[i] for i in range(len(offspring))]
+
 def surviorSelection(parents, offspring):
     return parents
-    
+
 if __name__ == '__main__':
 
     numParents = 3
@@ -50,6 +74,7 @@ if __name__ == '__main__':
     x1Range = (-3, 12)
     x2Range = (4, 6)
     initialSigma = 1.0
+    terminationCount = 10
 
     parents = createGeneration0(numParents, x1Range, x2Range, initialSigma)
     print parents
@@ -57,6 +82,9 @@ if __name__ == '__main__':
     offspring = recombination(parents, numVariables, numOffspring, 
         discreteGlobalRecombination, intermediateGlobalRecombination)
     print offspring
+
+    mutatedOffspring = mutation(offspring, 2, numVariables)
+    print mutatedOffspring
     # create x(3) parents (values with default sigma of 1)
 
 
