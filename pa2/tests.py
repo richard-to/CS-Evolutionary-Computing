e2ca1932bf8cadc7dcd528f8069ac5b84c66574c
richard-to <gohn76@gmail.com>
2013-03-08 18:22:18 -0900

    Add unit tests for es helper functions

--- pa2/tests.py
+++ pa2/tests.py
@@ -2,3 +2,24 @@ import es
 import unittest
 
 class TestEs(unittest.TestCase):
+
+    def testTranspose(self):
+        matrix = [[1, 3, 5], [2, 4, 6]]
+        self.assertEqual(es.transpose(es.transpose(matrix)), matrix)
+
+    def testSliceMatrix(self):
+        matrix = [[1, 3, 5], [2, 4, 6]]
+        left, right = es.sliceMatrix(matrix, 2)
+        self.assertEqual(left, [[1, 3], [2, 4]])
+        self.assertEqual(right, [[5], [6]])
+
+    def testCheckRange(self):
+        range = [3, 6]
+        self.assertTrue(es.checkRange(3, *range))
+        self.assertTrue(es.checkRange(6, *range))
+        self.assertTrue(es.checkRange(4, *range))
+        self.assertFalse(es.checkRange(8, *range))
+
+
+if __name__ == '__main__':
+    unittest.main()
