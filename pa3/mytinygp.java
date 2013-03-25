cdd44f19d7079625b1f5ccc64768e5688ea8331b
richard-to <gohn76@gmail.com>
2013-03-25 01:48:20 -0800

    Switch back to random seed

--- pa3/mytinygp.java
+++ pa3/mytinygp.java
@@ -390,7 +390,7 @@ public class MyTinyGp
 
     public static void main(String[] args) {
         String fname = "problem.dat";
-        long s = 1;
+        long s = -1;
         
         if (args.length == 2) {
             s = Integer.valueOf(args[0]).intValue();
