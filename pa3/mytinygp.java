510f3038381b475ea1ed5ce7f5a098bf792a2ba7
richard-to <gohn76@gmail.com>
2013-03-24 20:25:31 -0800

    Add initial tinygp code with comments citing original source

---
+++ pa3/mytinygp.java
@@ -0,0 +1,10 @@
+/**
+ * Implementation of tiny gp that evolves a program 
+ * representing a booealn expression.
+ *
+ * Source modified from tinygp originally developed by 
+ * Riccardo Poli (http://cswww.essex.ac.uk/staff/rpoli/TinyGP/)
+ *
+ * Author: Richard To
+ * Date: 2/24/13
+ */
